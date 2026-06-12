797d5cdd654e01110d37b5fe70eb17f02eef88ab
MUHAMMAD BAGAS WAHYUDI <126631168+Bagaswhyd@users.noreply.github.com>
2026-06-12 17:02:46 +0700

    Add files via upload

---
+++ Workflow-CI/MLProject/modelling.py
@@ -0,0 +1,12 @@
+Computing Project 3 sks
+kerja praktik 3 sks
+penulisan proposal 2 sks
+matkul pilihan 1 3 sks (Pengolahan Citra Digital)
+matkul pilihan 2 3 sks (visi computer)
+matkul pilihan 3 3 sks (
+Teori Bahasa dan Automata 2 sks
+
+19 sks
+115 + 19 = 134
+
+3 3 4 = 10 
